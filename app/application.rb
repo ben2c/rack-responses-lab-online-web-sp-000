9987f607706c060c6fb9f456159e2e6ff81d8e18
Benjamin Cheng <bencheng200@gmail.com>
2020-12-21 20:28:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,11 @@ class Application
 
     time = Time.now.hour
 
-
+    if time < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
 
     resp.finish
   end
