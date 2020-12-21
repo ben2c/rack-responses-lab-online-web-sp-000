8c18696d2a949b695ce194805d5c0c5c6e80c84f
Benjamin Cheng <bencheng200@gmail.com>
2020-12-21 20:26:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.hour
+    time = Time.new
+    timenow = time.hour
     resp.write time
 
     if num_1==num_2 && num_2==num_3
