f39aa19245e0b91b0128a12d57d7ca246dfdc749
Benjamin Cheng <bencheng200@gmail.com>
2020-12-21 20:27:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,15 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.new
-    timenow = time.hour
-    resp.write timenow
+    time = Time.now.hour
 
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
-    end
+
 
     resp.finish
   end
